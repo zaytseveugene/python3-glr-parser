f4f20b2576e38d8706d49fac8051c2405240572a
Eugene Zaytsev <chipozzo@yandex.ru>
2016-02-17 10:39:49 +0300

    no message

---
+++ example.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from glr_parser.glr import GLRParser
+
+dictionaries = {
+    u"CLOTHES": [u"куртка", u"пальто", u"шубы"]
+}
+
+grammar = u"""
+    S = adj<agr-gnc=1> CLOTHES
+"""
+
+glr = GLRParser(grammar, dictionaries=dictionaries, debug=False)
+
+text = u"на вешалке висят пять красивых курток и вонючая шуба"
+for parsed in glr.parse(text):
+    print("FOUND:", parsed)
